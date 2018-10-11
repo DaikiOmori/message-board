b9d20c5131187cc9509adb15bc1ccebbbfb83494
daiki.omori <daiki0619@gmail.com>
2018-10-11 09:34:24 +0000

    DRY

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,60 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    set_message
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+     set_message
+  end
+
+  def update
+    set_message
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    set_message
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+  end
+
+private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
