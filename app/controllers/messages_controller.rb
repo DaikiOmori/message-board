ec1a147d0ee09b4199b73cc6099024153704277a
daiki.omori <daiki0619@gmail.com>
2018-10-12 14:02:22 +0000

    bootstrap design

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(title: 'test title 1', content: 'test content 1')
+Message.create(title: 'test title 2', content: 'test content 2')
+Message.create(title: 'test title 3', content: 'test content 3')
+(1..100).each do |number|
+  Message.create(title: 'test title' + number.to_s, content: 'test content' + number.to_s)
+end
